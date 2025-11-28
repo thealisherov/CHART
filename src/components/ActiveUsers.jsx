49756fa3580b64854df2a937f5e914d98f25fccb
samandarjumayev <samandarjumayev005@gmail.com>
2025-11-28 14:43:44 +0500

    active users page completed
    Yana o'zgarish qilishim mumkin

--- src/components/ActiveUsers.jsx
+++ src/components/ActiveUsers.jsx
@@ -1,6 +1,40 @@
+import activeUsersData from "../json/active_users.json";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  LineElement,
+  PointElement,
+  LinearScale,
+  CategoryScale,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(
+  LineElement,
+  PointElement,
+  LinearScale,
+  CategoryScale,
+  Tooltip,
+  Legend
+);
+
+const data = {
+    labels: activeUsersData.labels,
+    datasets: [
+      {
+        label: activeUsersData.title,
+        data: activeUsersData.values,
+        borderWidth: 1,
+        borderColor: 'dodgerblue',
+        backgroundColor: 'rgba(54, 162, 235, 0.5)',
+      },
+    ],
+  };
 
 export default function ActiveUsers(){
-    return <div>
-        Active Users
+    return <div className="flex flex-col items-center justify-center mt-10">
+        <h1 className="bg-[#4AC0FF] py-2 w-full rounded-lg border-b-2 border-blue-700 text-center text-2xl font-semibold shadow">Active Users</h1>
+        <Line key={Math.random()} data={data} />
     </div>
 }
